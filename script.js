a0c6976538a84699d05146725e2fe23bc03f506a
paulorcmendes <mendesdpn@gmail.com>
2016-07-12 10:40:44 -0300

    Leitura de xml funcionando. Testes funcionando

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function openCode(language){
     document.getElementById(language).style.display = "block";
 }
 
-function test(){
+/*function test(){
 	var input = document.getElementsByClassName("input");
 	var a = Number(document.getElementById("txtA").value);
 	var b = Number(document.getElementById("txtB").value);
@@ -26,14 +26,14 @@ function test(){
 		}
 	}
 }
-
+*/
 function initValues(){
 	var ipts = document.getElementsByClassName("variableInput");
 	for (var i = 0; i < ipts.length; i++) {
 		ipts[i].value = 0;
 	}
 }
-
+/*
 function correct(a, b, c){
 	var maior = a;
 	if(b > maior){
@@ -56,7 +56,7 @@ function wrong(a, b, c){
 	}
 
 }
-
+*/
 
 
 function loadXMLDoc(dname){
@@ -73,18 +73,20 @@ function loadXMLDoc(dname){
     xhttp.send();
     return xhttp.responseXML;
 }
-function myFunction() {
+function myFunction(index) {
 
 	var x;
     var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/85412057/question.xml"); 
     dataBase = xmlDoc.getElementsByTagName("dataBase")[0];
-    question = dataBase.getElementsByTagName("question")[1];
+    question = dataBase.getElementsByTagName("question")[index];
     description = question.getElementsByTagName("description")[0].childNodes[0].nodeValue;
     code = question.getElementsByTagName("code");
     entry = question.getElementsByTagName("entry");
+    test = question.getElementsByTagName("test")[0].childNodes[0].nodeValue;
     
     
-    html = "<div class = \"desc\">"+description+"</div>";
+    html = "<div class = \"desc\">"+description+"</div>";    
+
     html+= "<div class=\"tabs\">"
     for (var i = 0; i < code.length; i++) {
     	html+= "<a href=\"#\" onclick=\"openCode('"+code[i].getAttribute('language')+"');\">"+code[i].getAttribute('language')+"</a>";
@@ -103,34 +105,46 @@ function myFunction() {
     	html+= "<label>"+entry[i].childNodes[0].nodeValue+"</label>";
     	html+= "<input type=\"text\" class = \"variableInput\" id=\"txt"+entry[i].childNodes[0].nodeValue+"\"><br>";
     }
-    html += "<input type=\"submit\" value=\"Submit\" onclick=\"test();\">";
+    html += "<input type=\"submit\" value=\"Submit\" onclick=\"test()\">";
+
     html += "</div>";
    
-	html += "<script>";
-	html += "function test(){ var input = document.getElementsByClassName(\"input\");";
+	
+    document.getElementById("question").innerHTML = html;
+
+    html = "function test(){ var input = document.getElementsByClassName(\"input\");";
 	for (var i = 0; i < entry.length; i++) {
 		html+= " var "+entry[i].childNodes[0].nodeValue + " = Number(document.getElementById(\"txt"+entry[i].childNodes[0].nodeValue+"\").value);";
 	}
 
-	/*	var a = Number(document.getElementById("txtA").value);
-		var b = Number(document.getElementById("txtB").value);
-		var c = Number(document.getElementById("txtC").value);
-	
-		if(isNaN(a) || isNaN(b) || isNaN(c)){
-			window.alert("Entrada Invalida");
-			initValues();
-		}else{
-			if(correct(a, b, c) != wrong(a, b, c)){
-				window.alert("Muito Bem");
-			}else{
-				window.alert("Tente Novamente");
-				initValues();
-			}
-		}
-	}*/
-	html += "</script>";
-    document.getElementById("question").innerHTML = html;
+	html += "if(";
+	for (var i = 0; i < entry.length-1; i++) {
+		html+= " isNaN("+entry[i].childNodes[0].nodeValue + ") ||";
+	}
+	html+= " isNaN("+entry[entry.length-1].childNodes[0].nodeValue + "))";
+
+	//provisório
+	html+="{ window.alert(\"Entrada Invalida\"); initValues();}";
+
+	html+="else{ if(correct("
+	for (var i = 0; i < entry.length-1; i++) {
+		html+= " "+entry[i].childNodes[0].nodeValue + ", ";
+	}
+	html+= entry[entry.length-1].childNodes[0].nodeValue + ") != wrong(";
+	for (var i = 0; i < entry.length-1; i++) {
+		html+= " "+entry[i].childNodes[0].nodeValue + ", ";
+	}
+	html+= entry[entry.length-1].childNodes[0].nodeValue + ")){";
+
+	//provisório
+	html+= "window.alert(\"Muito Bem\");";
+
+	html+=" }else{ ";
+	html+=" window.alert(\"Tente Novamente\"); initValues(); "
+	html+="} } }"
 
 
+	html += test;
+	document.getElementById("scriptMaroto").innerHTML = html;
 }
 
