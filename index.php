005698a5613ff4d738f47fa2bdacf43b500dbd60
paulorcmendes <mendesdpn@gmail.com>
2016-09-14 21:30:20 -0300

    Mudança para php funcional

--- index.php
+++ index.php
@@ -1,69 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<script src = "script.js"></script>	
-	<script type="text/javascript" id = "scriptMaroto"></script>
-	<title>Caça Bugs</title>
-
-	<link rel="stylesheet" href="style.css">	
- 	
-
-</head>
-<body>
-
-	<div id = "question" class =  "total"><!--
-		<div class="desc">
-		O código abaixo deveria retornar o maior dentre três números inteiros. Entretanto, há um erro. Você consegue inserir entradas que façam o código retornar uma resposta errada?
-		</div>
-		<div class="tabs">
-			<a href="#" onclick="openCode('lua');">Lua</a>
-			<a href="#" onclick="openCode('c');">C</a>
-		</div>
-		
-		<div id = "lua" class="code">
-function max3(a, b, c)
-	if a>b and a>c then
-		return a
-	elseif b>a and b>c then
-		return b
-	else
-		return c
-	end
-end		
-		</div>
-
-		<div id = "c" class="code" >
-int max3(int a, int b, int c){
-	if (a>b && a>c){
-		return a;
-	}else if(b>a && b>c){
-		return b;
-	}else{
-		return c;
-	}
-}
-		</div>
-		
-		<div class="input">
-			<form>
-				<label>A</label>
-				<input type="text" class = "variableInput" id="txtA"><br>
-				<label>B</label>
-				<input type="text" class = "variableInput" id="txtB"><br>
-				<label>C</label>
-				<input type="text" class = "variableInput" id="txtC"><br>
-				<input type="submit" value="Submit" onclick="test();">
-			</form>
-		</div>
-	</div>
-	<p id = "demo">
-		
-	</p>-->
-		<script>
-		//myFunction(0);
-		//myFunction(1);
-		</script>
-	</div>
 	<?php
 	$xml=simplexml_load_file("question.xml") or die("Error: Cannot create object");
 	$index = 0;
@@ -72,6 +11,7 @@ int max3(int a, int b, int c){
 		$html .= " function test".$index."(){ var img; var flag = false;";
 		$test = $question->test;
 		$code = $question->code;
+		$entry = $question->entry;
 	    foreach ($question->entry as $variableName) {
 	    	$type = $variableName["type"];
 	    	if($type == "int"){
@@ -85,23 +25,23 @@ int max3(int a, int b, int c){
 				$html.= " var ".$variableName. " = Number(document.getElementById(\"txt".$variableName."\").value);";
 				$html.= "if (isNaN(".$variableName.")) { flag = true;}";
 			}else if($type == "vector"){
-				$html.= " var ".$variableName. " = document.getElementById(\"txt".$variableName."\").value.split(\" \");";
-				$html.= " for(i = 0; i < ".$variableName.".length; i++){";
-				$html.= " if(".$variableName."[i] == \" \" || ".$variableName."[i] == \"\"){";
-				$html.= "flag = true; break;}";
-				$html.= $variableName."[i] = Number(".$variableName+"[i]);";
-				$html.= "if (isNaN(".$variableName."[i])) { flag = true; break; } }";
+				$html.= " var ".$variableName. " = document.getElementById(\"txt".$variableName."\").value.split(\" \"); ";
+				$html.= " for(i = 0; i < ".$variableName.".length; i++){ ";
+				$html.= " if(".$variableName."[i] == \" \" || ".$variableName."[i] == \"\"){ ";
+				$html.= "flag = true; break;} ";
+				$html.= $variableName."[i] = Number(".$variableName."[i]); ";
+				$html.= " if(isNaN(".$variableName."[i])) { flag = true; break; } } ";
 			}else{
 				$html.= " var ".$variableName. " = document.getElementById(\"txt".$variableName."\").value;";
 			}
 	    }
 	    $html.="if(flag){ window.alert(\"Entrada Invalida\"); initValues(); img = \"\";}";
 
-		$html.="else{ if(correct(";
+		$html.="else{ if(correct".$index."(";
 		for ($i = 0; $i < sizeof($entry)-1; $i++) {
 			$html.= " ".$entry[$i] . ", ";
 		}
-		$html.= $entry[sizeof($entry)-1] . ") != wrong(";
+		$html.= $entry[sizeof($entry)-1] . ") != wrong".$index."(";
 		for ($i = 0; $i < sizeof($entry)-1; $i++) {
 			$html.= " ".$entry[$i] . ", ";
 		}
@@ -116,9 +56,27 @@ int max3(int a, int b, int c){
 
 		$html .= $test;
 		
-		$html .= " openCode(\"".$code['language']."\");";
-		echo $html;
+		//$html .= " openCode(\"".$code['language']."\");";
+		$myfile = fopen("scriptQuestions.js", "w") or die("Unable to open file!");
+		fwrite($myfile, $html);
+		fclose($myfile);
+		//echo $html;
+		$index++;
 	} 
 	?>
+	<script src = "scriptQuestions.js"></script>
+	<title>Caca Bugs</title>
+
+	<link rel="stylesheet" href="style.css">	
+ 	
+
+</head>
+<body>
+
+	<div id = "question" class =  "total">
+		<script>
+		myFunction(0);
+		</script>
+	</div>	
 </body>
 </html>
--- script.js
+++ script.js
@@ -8,56 +8,12 @@ function openCode(language){
     document.getElementById(language).style.display = "block";
 }
 
-/*function test(){
-	var input = document.getElementsByClassName("input");
-	var a = Number(document.getElementById("txtA").value);
-	var b = Number(document.getElementById("txtB").value);
-	var c = Number(document.getElementById("txtC").value);
-
-	if(isNaN(a) || isNaN(b) || isNaN(c)){
-		window.alert("Entrada Invalida");
-		initValues();
-	}else{
-		if(correct(a, b, c) != wrong(a, b, c)){
-			window.alert("Muito Bem");
-		}else{
-			window.alert("Tente Novamente");
-			initValues();
-		}
-	}
-}
-*/
 function initValues(){
 	var ipts = document.getElementsByClassName("variableInput");
 	for (var i = 0; i < ipts.length; i++) {
 		ipts[i].value = "";
 	}
 }
-/*
-function correct(a, b, c){
-	var maior = a;
-	if(b > maior){
-		maior = b;
-	}
-	if(c > maior){
-		maior = c;
-	}
-	return maior;
-}
-
-function wrong(a, b, c){
-
-	if (a>b && a>c){
-		return a;
-	}else if(b>a && b>c){
-		return b;
-	}else{
-		return c;
-	}
-
-}
-*/
-
 
 function loadXMLDoc(dname){
     if (window.XMLHttpRequest)
@@ -74,9 +30,8 @@ function loadXMLDoc(dname){
     return xhttp.responseXML;
 }
 function myFunction(index) {
-
 	var x;
-    var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/85412057/question.xml"); 
+    var xmlDoc = loadXMLDoc("question.xml"); 
     dataBase = xmlDoc.getElementsByTagName("dataBase")[0];
     question = dataBase.getElementsByTagName("question")[index];
     description = question.getElementsByTagName("description")[0].childNodes[0].nodeValue;
@@ -113,58 +68,5 @@ function myFunction(index) {
 	
 	
     document.getElementById("question").innerHTML = html;
-    
-    html = "function test"+index+"(){ var img; var flag = false;";
-	for (var i = 0; i < entry.length; i++) {
-		var variableName = entry[i].childNodes[0].nodeValue;
-		var type = entry[i].getAttribute('type');
-		//caso particular de teste de cada tipo
-		if(type == "int"){
-			html+= " var "+variableName+ " = document.getElementById(\"txt"+variableName+"\").value;";
-			html+= "if ("+variableName+" == \"\") { flag = true;}"
-			html+= " var "+variableName+ " = Number(document.getElementById(\"txt"+variableName+"\").value);";
-			html+= "if (isNaN("+variableName+") || !Number.isInteger("+variableName+")) { flag = true;}"
-		}else if(type == "float"){
-			html+= " var "+variableName+ " = document.getElementById(\"txt"+variableName+"\").value;";
-			html+= "if ("+variableName+" == \"\") { flag = true;}"
-			html+= " var "+variableName+ " = Number(document.getElementById(\"txt"+variableName+"\").value);";
-			html+= "if (isNaN("+variableName+")) { flag = true;}"
-		}else if(type == "vector"){
-			html+= " var "+variableName+ " = document.getElementById(\"txt"+variableName+"\").value.split(\" \");";
-			html+= " for(i = 0; i < "+variableName+".length; i++){"
-			html+= " if("+variableName+"[i] == \" \" || "+variableName+"[i] == \"\"){";
-			html+= "flag = true; break;}"
-			html+= variableName+"[i] = Number("+variableName+"[i]);"
-			html+= "if (isNaN("+variableName+"[i])) { flag = true; break; } }";
-		}else{
-			html+= " var "+variableName+ " = document.getElementById(\"txt"+variableName+"\").value;";
-		}
-	}
-
-	//provisório
-	html+="if(flag){ window.alert(\"Entrada Invalida\"); initValues(); img = \"\";}";
-
-	html+="else{ if(correct("
-	for (var i = 0; i < entry.length-1; i++) {
-		html+= " "+entry[i].childNodes[0].nodeValue + ", ";
-	}
-	html+= entry[entry.length-1].childNodes[0].nodeValue + ") != wrong(";
-	for (var i = 0; i < entry.length-1; i++) {
-		html+= " "+entry[i].childNodes[0].nodeValue + ", ";
-	}
-	html+= entry[entry.length-1].childNodes[0].nodeValue + ")){";
-
-	//provisório
-	html+= "myFunction("+(index+1)+"); return ; /*window.alert(\"Muito Bem\");*/ img = \"<img src= \'http://i.makeagif.com/media/11-15-2015/4lDs7n.gif\' alt=\'Result\' width=\'100%\' height=\'100%\'>\";";
-	///html+=" myFunction("+(index+1)+");";
-	html+=" }else{ ";
-	html+=" /*window.alert(\"Tente Novamente\");*/ initValues(); img = \"<img src=\'http://vandalsbucket.s3-sa-east-1.amazonaws.com/spree/products/45992/large/Errou.jpeg?1439700751\' alt=\'Result\' width=\'100%\' height=\'100%\'>\";";
-	html+="} } document.getElementById(\"myResult\").innerHTML = img;}"
-
-	html += test;
-	
-	html+= " openCode(\""+code[0].getAttribute('language')+"\");";
-	
-	document.getElementById("scriptMaroto").innerHTML = html;
 }	
 
