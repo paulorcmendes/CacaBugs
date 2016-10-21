07f3046d2d1760387236b6e488878efd472c6ba8
paulorcmendes <mendesdpn@gmail.com>
2016-10-21 20:22:34 -0300

    adição botão de logout

--- index.php
+++ index.php
@@ -18,12 +18,15 @@
 </head>
 <body>	
 	<div class = "logo">
+	  <a href="logout.php" id = "logout" class="submit">Logout</a>
 	  <a href=""><img class = "imgLogo" src="logo.png"></a>
 	  <a href="https://telemidiaufma.slack.com"><img class = "parceiro" src="parceiro.png"></a>
-	</div>
+
+	</div>	
 	<div id = "divTotal" class="total">
-			
+		
 	</div>
+
 	<div id="overlay-back"></div>
 </body>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
