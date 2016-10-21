7703bc09953667ab3b8b6342cb3feea210f829bd
paulorcmendes <mendesdpn@gmail.com>
2016-10-21 11:06:53 -0300

    mudança em usuário logado

--- sendData.php
+++ sendData.php
@@ -1,4 +1,5 @@
 <?php
+include('session.php');
 $question = 0;
 if(isset($_POST['currentQuestion'])){
     $question = $_POST['currentQuestion'];
@@ -8,8 +9,8 @@ $con = mysqli_connect('localhost','root','','caca_bugs');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
-
-$sql = "UPDATE user SET question =".$question." WHERE id=1";
+$user_check = $login_session;
+$sql = "UPDATE user SET question = ".$question." WHERE email = '$user_check'";
 
 if (mysqli_query($con, $sql)) {
     echo "Record updated successfully";
