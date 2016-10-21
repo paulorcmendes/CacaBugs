148a63ba53730a1c4a9aab0ad59f4c3e9b995ed7
paulorcmendes <mendesdpn@gmail.com>
2016-10-21 10:38:48 -0300

    salvando dados no banco com sucesso

--- sendData.php
+++ sendData.php
@@ -1,7 +1,20 @@
 <?php
+$question = 0;
 if(isset($_POST['currentQuestion'])){
     $question = $_POST['currentQuestion'];
-
-    echo $question+32;
-
 }
+
+$con = mysqli_connect('localhost','root','','caca_bugs');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+$sql = "UPDATE user SET question =".$question." WHERE id=1";
+
+if (mysqli_query($con, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($con);
+}
+
+mysqli_close($con);
