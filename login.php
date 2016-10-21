4aabf4c3c9ab775ec44397d8128002214d8a0bfe
paulorcmendes <mendesdpn@gmail.com>
2016-10-21 20:25:50 -0300

    md5 cripto

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM user WHERE email = '$myusername' and senha = '$mypassword'";
+      $sql = "SELECT id FROM user WHERE email = '$myusername' and senha = MD5('$mypassword')";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
