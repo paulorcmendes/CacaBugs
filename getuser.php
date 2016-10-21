7a4904bc253331c7b9a297fd696407d78130cf7f
paulorcmendes <mendesdpn@gmail.com>
2016-10-21 11:22:15 -0300

    pegando dados do usuário logado

--- getuser.php
+++ getuser.php
@@ -1,34 +1,21 @@
 <?php
-//$q = intval($_GET['q']);
+include('session.php');
 $con = mysqli_connect('localhost','root','','caca_bugs');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
 mysqli_select_db($con,"ajax_demo");
-//$sql="SELECT * FROM user WHERE email = '".$q."'";
-$sql="SELECT * FROM user";
+$user_check = $login_session;
+$sql="SELECT * FROM user WHERE email = '$user_check'";
 $result = mysqli_query($con,$sql);
 
-// echo "<table>
-// <tr>
-// <th>id</th>
-// <th>email</th>
-// <th>nome</th>
-// </tr>";
-// while($row = mysqli_fetch_array($result)) {
-//     echo "<tr>";
-//     echo "<td>" . $row['id'] . "</td>";
-//     echo "<td>" . $row['email'] . "</td>";
-//     echo "<td>" . $row['nome'] . "</td>";
-//     echo "</tr>";
-// }
-// echo "</table>";
+
 
 $arrayReturn = array();
 
 while($row = mysqli_fetch_array($result)) {
-    $arrayReturn[] = array('id' => $row['id'], 'nome' => $row['nome']);
+    $arrayReturn[] = array('nome' => $row['nome']);
 }
 
 echo json_encode($arrayReturn);
