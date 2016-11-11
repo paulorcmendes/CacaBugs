149af3d40d38d2e8bbecd9ae3846f3a78c7f82cb
paulorcmendes <mendesdpn@gmail.com>
2016-11-11 10:29:32 -0300

    Mudança no nome das tabelas e default para sendDate

--- caca_bugs.sql
+++ caca_bugs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 04-Nov-2016 às 14:02
--- Versão do servidor: 10.1.13-MariaDB
--- PHP Version: 7.0.6
+-- Host: localhost
+-- Generation Time: Nov 11, 2016 at 02:27 
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,21 +23,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `log`
+-- Table structure for table `log`
 --
 
 CREATE TABLE `log` (
   `id` int(11) NOT NULL,
   `id_user` int(200) NOT NULL,
-  `data` date NOT NULL,
-  `right` tinyint(1) NOT NULL DEFAULT '0',
+  `sendDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isRight` tinyint(1) NOT NULL DEFAULT '0',
   `question` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `log`
+--
+
+INSERT INTO `log` (`id`, `id_user`, `sendDate`, `isRight`, `question`) VALUES
+(2, 1, '2016-11-11 10:15:43', 1, 1),
+(3, 1, '2016-11-11 10:16:45', 0, 4),
+(4, 1, '2016-11-11 10:19:09', 1, 1),
+(5, 1, '2016-11-11 10:19:35', 0, 0),
+(6, 3, '2016-11-11 10:20:31', 1, 0),
+(7, 3, '2016-11-11 10:20:38', 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -49,12 +61,12 @@ CREATE TABLE `user` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `email`, `nome`, `senha`, `question`) VALUES
-(1, 'mendes', 'paulo', '8dc05a2dd9ab9c89fdd980fcea730391', 1),
-(3, 'csalles', 'Carlos de Salles', '419789426eeccf07e77dae48ce291063', 0),
+(1, 'mendes', 'paulo', '8dc05a2dd9ab9c89fdd980fcea730391', 4),
+(3, 'csalles', 'Carlos de Salles', '419789426eeccf07e77dae48ce291063', 2),
 (4, 'pedropva', 'pedro', 'a6c4fc689f874ebf8c957113b1266e22', 1);
 
 --
@@ -81,7 +93,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `log`
 --
 ALTER TABLE `log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `user`
 --
