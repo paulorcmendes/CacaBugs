c3ca6bcfd2b5e14a68175c8749105f17fd99f9c6
paulorcmendes <mendesdpn@gmail.com>
2017-11-06 17:31:57 -0300

    Sign Up added

--- signUp.php
+++ signUp.php
@@ -1,43 +1,41 @@
 <?php
    include("config.php");
-   session_start();
+   //session_start();
    $error = "";
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM user WHERE email = '$myusername' and senha = md5('$mypassword')";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         //session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: index.php");
-      }else {
-         $error = "Your Email or Password is invalid";
+      $mypasswordR = mysqli_real_escape_string($db,$_POST['passwordR']);
+      if($myusername != ""){
+        if($mypassword != "" && $mypassword == $mypasswordR){
+          $sql = "INSERT INTO user(email, nome, senha, question) values ('$myusername', '$myusername', MD5('$mypassword'), '0')";
+          
+          if (mysqli_query($db, $sql)) {
+              header('location: login.php');
+          } else {
+              echo "Error updating record: " . mysqli_error($db);
+          }
+        }
+        else{
+          $error = "Passords do not match";
+        }
+      }else{
+        $error = "Insert a valid email!";
       }
    }
 ?>
 <html>
    
    <head>
-      <title>Login Page</title>
+      <title>Create Account</title>
       
       <link rel="stylesheet" href="style.css">
       
    </head>
    
    <body bgcolor = "#FFFFFF">
-	
       <div class = "logo">
         <a href=""><img class = "imgLogo" src="logo.png"></a>
         <a href="https://telemidiaufma.slack.com"><img class = "parceiro" src="parceiro.png"></a>
@@ -46,7 +44,7 @@
       <div id = "divTotal" class="total">
         <div align = "center">
          <div style = "" align = "left">
-            <div style = "font-size:20px; background-color:#333333; color:#FFFFFF; padding:3px;"><b>Página de Login</b></div>
+            <div style = "font-size:20px; background-color:#333333; color:#FFFFFF; padding:3px;"><b>Criar Conta</b></div>
         
             <div class= "container" style = "font-size:15px; margin:30px">
                
@@ -59,17 +57,22 @@
                     <label>Senha:</label><br /><br />
                     <input class="variableInput" type = "password" name = "password" class = "box" /><br/><br />
                   </div>
-                  <input class="submit" type = "submit" value = "Login"/><br />
+                  <div class="form-group">
+                    <label>Repita a Senha:</label><br /><br />
+                    <input class="variableInput" type = "password" name = "passwordR" class = "box" /><br/><br />
+                  </div>
+                  <input class="submit" type = "submit" value = "Criar"/><br />
                </form>
                
                <div style = "font-size:18px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-              
-              
+          
+            </div>
         
          </div>
-         <a class="submit" href="signUp.php">Criar Nova Conta</a>
+      
         </div>
-      </div>   
+      </div>	     
+      
 
    </body>
 </html>
